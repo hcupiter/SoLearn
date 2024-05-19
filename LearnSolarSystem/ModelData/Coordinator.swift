a78f5bddc43247769214732d029e590e87a6f42a
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-05-19 20:38:59 +0700

    Added TapGesture Event Handler

---
+++ LearnSolarSystem/ModelData/Coordinator.swift
@@ -0,0 +1,29 @@
+//
+//  Coordinator.swift
+//  LearnSolarSystem
+//
+//  Created by Hans Arthur Cupiterson on 19/05/24.
+//
+
+import Foundation
+import RealityKit
+import SwiftUI
+
+class Coordinator: NSObject {
+    var parent: ARViewContainer
+    
+    init(_ parent: ARViewContainer){
+        self.parent = parent
+    }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        guard let arView = sender.view as? ARView else { return }
+        
+        // handle the tap on the entity
+        let tapLocation = sender.location(in: arView)
+        if let modelEntity = arView.entity(at: tapLocation) {
+            // handle logic on the tapped entity
+            print("[DEBUG] Tapped on entity: \(modelEntity.name)")
+        }
+    }
+}
--- LearnSolarSystem/View/ARViewContainer.swift
+++ LearnSolarSystem/View/ARViewContainer.swift
@@ -15,17 +15,41 @@ struct ARViewContainer: UIViewRepresentable {
     let arView = ARView(frame: .zero)
     
     // Create initial horizontal plane anchor for the content
-    let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
+    let anchor = AnchorEntity()
     
     func makeUIView(context: Context) -> ARView {
+        arView.debugOptions = .showWorldOrigin
+        addAnchor()
         return arView
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
+    
+    func setupTapGestureRecognizer(context: Context){
+        let tapGesture = UITapGestureRecognizer(
+            target: context.coordinator,
+            action: #selector(context.coordinator.handleTap(_:))
+        )
+        arView.addGestureRecognizer(tapGesture)
+    }
     
     func addAnchor(){
         // Add the horizontal plane anchor to the scene
         arView.scene.anchors.append(anchor)
     }
     
+    // overided function to communicate with SwiftUI
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func addDebugBox(){
+        // Create a cube model
+        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
+        let material = SimpleMaterial(color: .green, roughness: 0.15, isMetallic: true)
+        let model = ModelEntity(mesh: mesh, materials: [material])
+        model.transform.translation.y = 0.05
+        
+        anchor.children.append(model)
+    }
 }
